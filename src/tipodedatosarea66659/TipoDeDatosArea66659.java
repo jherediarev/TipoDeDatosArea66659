78408fcbbf0dac146c747e12d99446f817fcea2b
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-27 20:47:01 -0600

    Área de rectangulo

---
+++ src/tipodedatosarea66659/TipoDeDatosArea66659.java
@@ -0,0 +1,33 @@
+/*
+ * UNIVERSIDAD AUTÓNOMA DE CAMPECHE
+ * Facultad de Ingeniería
+ * Ingeniería en Sistemas Computacionales
+ */
+package tipodedatosarea66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class TipoDeDatosArea66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // LADOS DEL RECTÁNGULO
+       
+        int lado1 = 17;
+        int lado2 = 29;
+        
+        // ÁREA DEL RECTÁNGULO bxa
+        
+        System.out.println("lado1 = " + lado1);
+        System.out.println("lado2 = " + lado2);
+        System.out.println("El area del rectangulo es " + lado1 * lado2);
+        
+        
+    }
+    
+}
